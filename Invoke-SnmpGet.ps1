78d890234a1679db6357303c93889a22170e63b7
Petter Bomban <petterbomban@gmail.com>
2017-02-01 12:25:58 +0100

    more changes

---
+++ Invoke-SnmpGet.ps1
@@ -0,0 +1,65 @@
+## Load SharpSnmpLib 
+[System.Reflection.Assembly]::LoadFrom(".\SharpSnmpLib.Full.dll") | Out-Null
+
+function Invoke-SnmpGet () {
+
+    # $OIDs can be a single OID string, or an array of OID strings
+    # $TimeOut is in msec, 0 or -1 for infinite
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $True)]
+        [string]$sIP,
+
+        [Parameter(Mandatory = $True)]
+        [string[]]$sOIDs = @(),
+
+        [Parameter(Mandatory = $True)]
+        [string]$Community,
+
+        [Parameter(Mandatory = $False)]
+        [int]$UDPport = 162,
+
+        [Parameter(Mandatory = $False)]
+        [int]$TimeOut = 3000
+    )
+
+    # Create OID variable list
+    $vList = New-Object 'System.Collections.Generic.List[Lextm.SharpSnmpLib.Variable]'
+    foreach ($sOID in $sOIDs)
+    {
+        $oid = New-Object Lextm.SharpSnmpLib.ObjectIdentifier ($sOID)
+        $vList.Add($oid)
+    }
+    
+    # Create endpoint for SNMP server
+    $ip = [System.Net.IPAddress]::Parse($sIP)
+    $svr = New-Object System.Net.IpEndPoint ($ip, 161)
+    
+    # Use SNMP v2
+    $ver = [Lextm.SharpSnmpLib.VersionCode]::V2
+    
+    # Perform SNMP Get
+    try
+    {
+        $msg = [Lextm.SharpSnmpLib.Messaging.Messenger]::Get($ver, $svr, $Community, $vList, $TimeOut)
+    }
+    catch
+    {
+        Write-Host "SNMP Get error: $_"
+        Return $null
+    }
+
+    $msg
+}
+<#
+$Get = Invoke-SnmpGet `
+    -sIP "192.168.0.1" `
+    -sOIDs $oids `
+    -Community "ikt-fag.no" `
+    -UDPport 162 `
+    -TimeOut 5000
+
+$Get
+#Send-ToInfluxDB -InfluxServer "192.168.0.30" -InfluxPort 8086 -InfluxDB "" -InfluxUser "" -InfluxPass "" -Data $Get
+#>
--- main.ps1
+++ main.ps1
@@ -1,68 +1,37 @@
-## Based on Invoke-SNMPget from https://vwiki.co.uk/SNMP_and_PowerShell
+## ASA only for now
+$AsaOid = @(
+    "1.3.6.1.4.1.9.9.48.1.1.1.5.1", ## mem.used
+    "1.3.6.1.4.1.9.9.48.1.1.1.6.1", ## mem.free
+    "1.3.6.1.4.1.9.9.109.1.1.1.1.3.1" ## cpu.usage-percent
+    ## interface.outside-in
+    ## interface.outside-out
+)
+$IosOid = @()
+$NxOs = @()
 
-## Load SharpSnmpLib 
-1..2 | foreach {
-    [System.Reflection.Assembly]::LoadFrom("C:\Users\admin\Documents\GitHub\posh-influx-snmp-cisco\SharpSnmpLib.Full.dll") | Out-Null
+## Devices and their OIDs
+$Devices = @{
+    "ASA" = @{
+        "IpAddress" = "192.168.0.1"
+        "OID" = $AsaOid
+    }
+    "IOS" = 1
+    "NX-OS" = 1
 }
 
-function Invoke-SnmpGet () {
-
-    # $OIDs can be a single OID string, or an array of OID strings
-    # $TimeOut is in msec, 0 or -1 for infinite
-    [CmdletBinding()]
+function Start-GatheringData
+{
     param
     (
         [Parameter(Mandatory = $True)]
-        [string]$sIP,
-
-        [Parameter(Mandatory = $True)]
-        [string[]]$sOIDs = @(),
-
-        [Parameter(Mandatory = $True)]
-        [string]$Community,
-
-        [Parameter(Mandatory = $False)]
-        [int]$UDPport = 162,
-
-        [Parameter(Mandatory = $False)]
-        [int]$TimeOut = 3000
+        [hashtable]$Devices
     )
 
-    # Create OID variable list
-    $vList = New-Object 'System.Collections.Generic.List[Lextm.SharpSnmpLib.Variable]'
-    foreach ($sOID in $sOIDs)
+    foreach ($Device in $Devices)
     {
-        $oid = New-Object Lextm.SharpSnmpLib.ObjectIdentifier ($sOID)
-        $vList.Add($oid)
+        #Write-Output $Device.Key
+        Write-Output $Device.OID
     }
-    
-    # Create endpoint for SNMP server
-    $ip = [System.Net.IPAddress]::Parse($sIP)
-    $svr = New-Object System.Net.IpEndPoint ($ip, 161)
-    
-    # Use SNMP v2
-    $ver = [Lextm.SharpSnmpLib.VersionCode]::V2
-    
-    # Perform SNMP Get
-    try
-    {
-        $msg = [Lextm.SharpSnmpLib.Messaging.Messenger]::Get($ver, $svr, $Community, $vList, $TimeOut)
-    }
-    catch
-    {
-        Write-Host "SNMP Get error: $_"
-        Return $null
-    }
-
-    $msg
 }
 
-$Get = Invoke-SnmpGet `
-    -sIP "192.168.0.1" `
-    -sOIDs $oids `
-    -Community "ikt-fag.no" `
-    -UDPport 162 `
-    -TimeOut 5000
-
-$Get
-#Send-ToInfluxDB -InfluxServer "192.168.0.30" -InfluxPort 8086 -InfluxDB "" -InfluxUser "" -InfluxPass "" -Data $Get
+Start-GatheringData -Devices $Devices
